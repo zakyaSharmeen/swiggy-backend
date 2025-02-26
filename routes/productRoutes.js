e08cabc921b0923ca8e2b3966d6cab689ea12b6b
zakya sharmeen <sakarim9124@gmail.com>
2025-02-26 23:24:01 +0530

    updated

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,7 @@
     const express = require('express');
     const router = express.Router();
     const productController = require('../controllers/productController');
+    const path = require("path");
 
     router.post('/add-product/:firmId', productController.addProduct);
     router.get('/:firmId/products', productController.getProductByFirm);
